b07fb3b77c8523b5578d0024126de0a9905013b7
Cath W <catkenna@hotmail.com>
2019-01-03 14:57:25 +0000

    Css updates with responsive code

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import TaskAdd from './Components/TaskAdd.js';
 import TaskTable from './Components/TaskTable.js';
 import Image from './Components/Image.js';
 import SubHeading from './Components/SubHeading.js';
-import NumberTasks from './Components/NumberTasks.js'
-
+import NumberTasks from './Components/NumberTasks.js';
 
 class App extends Component{
   render() {
     return (
       <div className="container">
-      
+
       <Header />
       <br/>
       <TaskAdd />
