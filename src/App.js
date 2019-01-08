10d2f19020d7a3aaa0410ab8dce05794f305d0d9
Cath W <catkenna@hotmail.com>
2019-01-08 22:04:13 +0000

    Task list- add task

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React, { Component } from 'react';
 import Header from './Components/Header.js';
-import SubmitButton from './Components/SubmitButton.js';
-import TaskAdd from './Components/TaskAdd.js';
-import TaskList from './Components/TaskList.js';
+
 import Image from './Components/Image.js';
 import SubHeading from './Components/SubHeading.js';
 import NumberTasks from './Components/NumberTasks.js';
 import './App.css'
+import TaskList from './Components/TaskList.js';
+import TaskEntry from './Components/TaskEntry.js'
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: []
+    };
+
+    this.addTask = this.addTask.bind(this);
+  }
+    
+  addTask(task) {
+    let currentListOfTasks = this.state.tasks;
+
+    currentListOfTasks.push(task);
+
+    this.setState({ tasks: currentListOfTasks
+    });
+  }
+  
 
 
 
-class App extends Component{
+
+
   render() {
     return (
       <div className="container">
-
-      <Header />
-      <br/>
-      <TaskAdd />
-      <br/> 
-
-      <SubmitButton />
-       <br/>
-
+        <Header />
+        
+        <TaskEntry tasks onSaveTaskHandler={this.addTask} /> 
+        <br/> 
        <SubHeading/>
        <br/>
        <NumberTasks/>
        <br/> <br/>
-       <TaskList/>
-
-        <br/>
-        <Image/> 
-
-      </div>
+      <TaskList tasks={this.state.tasks} />
+      
+         </div>
     );
-
   }
+
 }
-
-
 export default App;
