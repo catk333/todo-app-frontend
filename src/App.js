f1abd7acaefd7710a9493f2601be8c9cf917ea2c
Cath W <catkenna@hotmail.com>
2019-01-22 17:59:26 +0000

    For review only- not working

--- src/App.js
+++ src/App.js
@@ -4,40 +4,62 @@ import SubHeading from './Components/SubHeading.js';
 import NumberTasks from './Components/NumberTasks.js';
 import './App.css'
 import TaskList from './Components/TaskList.js';
-import TaskEntry from './Components/TaskEntry.js'
+import TaskEntry from './Components/TaskEntry.js';
+import Task from './Components/Task.js';
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      tasks: []
-    };
 
     this.addTask = this.addTask.bind(this);
-    this.removeTaskFromList = removeTaskFromList.bind(this);
-  }
+    this.removeTaskFromList=this.removeTaskFromList.bind(this);
+    this.addTaskCounter=this.addTaskCounter.bind(this);
+
+
+    this.state = {
+      tasks:[]
+      
+  };
+}
     
   addTask(task) {
     let currentListOfTasks = this.state.tasks;
 
     currentListOfTasks.push(task);
 
-    this.setState({ tasks:currentListOfTasks});
+    this.setState({tasks:currentListOfTasks});
   }
-
-  removeTaskFromList(task){
-
-    //set state of current task list
-    let currentListOfTasks = this.state.tasks;
+  
+  
+  removeTaskFromList(){
+    alert('Hello- you clicked remove!');
+}
+ 
+    //let currentListOfTasks = this.state.tasks;
 
     // use filter method to remove any books that are not matching the same taskDescription, and return the array - the task entered. 
-    let newTaskList = currentListOfTasks.filter( (task.taskDescription !== taskDescription));
+    //let newTaskList = currentListOfTasks.filter((tasks)=> tasks.description !== tasks.description);
 
-    //Reset state to show the new array as the list on the page. 
-    this.setState({tasks:newTaskList});
- }
+
+    //this.setState({tasks:newTaskList});
+ //};
   
+ addTaskCounter() {
+
+  //need to set the state of the tasks left to tasks in list
+  let tasksLeft = this.state.tasks;
+  
+  // need to get the length of the tasks array (.length)
+  let tasksRemaining = tasksLeft.length; 
+   
+  
+  // Set state of the tasks remaining into tasks?
+  this.setState({tasks:tasksRemaining})
+  };
+
+
   render() {
     return (
       <div className="container">
@@ -47,13 +69,14 @@ class App extends Component {
         <br/> 
        <SubHeading/>
        <br/>
-       <NumberTasks tasks={this.state.addTaskCounter}/>
+       <NumberTasks tasks={this.state.tasks} taskCounterHandler={this.addTaskCounter}/>
        <br/> <br/>
-      <TaskList tasks={this.state.tasks} removeTaskHandler={this.removeTaskFromList} />
-      
+      <TaskList tasks= {this.state.tasks}/>
+      <Task tasks={this.state.tasks} onRemoveTaskHandler={this.removeTaskFromList}/>
          </div>
     );
   }
 
 }
 export default App;
+
