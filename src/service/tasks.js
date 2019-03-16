e20b3a26b349df3b3a6336a9d8415c874c81b31d
Cath W <catkenna@hotmail.com>
2019-03-16 11:52:37 +0000

    axios task.js

--- src/service/tasks.js
+++ src/service/tasks.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
 
 const TasksService = {
-    async getTasks() {
+    async gettasks() {
         let res = await axios.get("https://a197h1xke1.execute-api.eu-west-2.amazonaws.com/dev/tasks");
-        let data = await res.data;
+        let data= await res.data;
         return data;
   
     }, 
 
-    async saveTask (task){
-        let res = await axios.post ("https://a197h1xke1.execute-api.eu-west-2.amazonaws.com/dev/tasks",task);
-        return res;
+    async saveTask(task){
+        let res = await axios.post("https://a197h1xke1.execute-api.eu-west-2.amazonaws.com/dev/tasks",task);
+        return res.data;
+    },
+
+
+   async deleteTask(taskId){
+    let res = await axios.delete("https://a197h1xke1.execute-api.eu-west-2.amazonaws.com/dev/tasks/{taskId}"+ taskId);
+    let data = await res.data;
+     return data;
+    },
+
+    async updateTask (taskId) {
+    let res = await axios.put ("https://a197h1xke1.execute-api.eu-west-2.amazonaws.com/dev/tasks{taskId}" + taskId);
+    let dataRecieved= await res.data;
+    return dataRecieved;
     }
-};
+}
+
 
 export default TasksService;
